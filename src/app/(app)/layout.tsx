7a20c3aba4cab49c5be8103b2aacc9d6663d92b9
fabricius gaspareti <fabriciusgaspareti@gmail.com>
2025-07-10 22:24:56 +0000

    preciso que diminua o tamanho desse container para deixar mais harmonico

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout({
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex-1 bg-background p-4 md:p-6 lg:p-8">
-        {children}
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
       </main>
     </div>
   );
