4c12dc674f7f70e1cba64e062b0cf2b6ff0d5986
fabricius gaspareti <fabriciusgaspareti@gmail.com>
2025-07-10 22:32:46 +0000

    agora diminuiu muito novamente, corrija encontrando um meio entre as 2 m

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout({
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex-1 bg-background p-4 md:p-6 lg:p-8">
-        <div className="mx-auto w-full max-w-6xl">{children}</div>
+        <div className="mx-auto w-full" style={{ maxWidth: '1440px' }}>{children}</div>
       </main>
     </div>
   );
